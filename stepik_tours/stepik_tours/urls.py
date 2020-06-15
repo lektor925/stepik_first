5edfeba42d30f5ff962060c7c10990838894115b
lektor925 <partizan925@mail.ru>
2020-06-15 16:35:06 +0300

    Fisrt commit

--- stepik_tours/stepik_tours/urls.py
+++ stepik_tours/stepik_tours/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from tours.views import MainView, DepartureView, TourView
 
 urlpatterns = [
-    path('', MainView),
-    path('/departure/<str:departure>/', DepartureView),
-    path('/tour/<int:id>/', TourView),
+    path('', MainView, name='home'),
+    path('departure/<str:departure>/', DepartureView),
+    path('tour/<int:id>/', TourView),
     path('admin/', admin.site.urls),
 ]
--- stepik_tours/tours/views.py
+++ stepik_tours/tours/views.py
@@ -1,13 +1,64 @@
+import random
+
 from django.shortcuts import render
+
+from .data import tours, title, subtitle, description, departures
 
 
 def MainView(request):
-    return render(request, 'tours/index.html')
+    main_tours = {}
+    while len(main_tours) < 6:
+        key = random.randint(1, len(tours))
+        if key not in main_tours:
+            main_tours[key] = tours[key]
+
+    data = {
+        'title': title,
+        'subtitle': subtitle,
+        'description': description,
+        'departures': departures,
+        'tours': main_tours
+    }
+    return render(request, 'tours/index.html', context=data)
 
 
 def DepartureView(request, departure):
-    return render(request, 'tours/departure.html')
+    departure_tours = {}
+    costs, nights = [], []
+
+    for key in tours:
+        if tours[key]['departure'] == departure:
+            departure_tours[key] = tours[key]
+            costs.append(tours[key]['price'])
+            nights.append(tours[key]['nights'])
+
+    page_departure = departures[departure].split()[-1]
+    min_cost = min(costs)
+    max_cost = max(costs)
+    min_nights = min(nights)
+    max_nights = max(nights)
+
+    data = {
+        'title': title,
+        'departures': departures,
+        'page_departure': page_departure,
+        'min_cost': min_cost,
+        'max_cost': max_cost,
+        'min_nights': min_nights,
+        'max_nights': max_nights,
+        'tours': departure_tours
+    }
+    return render(request, 'tours/departure.html', context=data)
 
 
 def TourView(request, id):
-    return render(request, 'tours/tour.html')
+    title_page = tours[id]['title'] + ' ' + ('★' * int(tours[id]['stars']))
+    departure = tours[id]['departure']
+    city = departures[departure].split(' ')[-1]
+    data = {
+        'title': title_page,
+        'departures': departures,
+        'city': city,
+        'tour': tours[id]
+    }
+    return render(request, 'tours/tour.html', context=data)
